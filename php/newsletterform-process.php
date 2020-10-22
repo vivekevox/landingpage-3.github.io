a864a935272d5e11e7177477298dc0a74257304a
vivekevox <68679832+vivekevox@users.noreply.github.com>
2020-10-22 14:31:45 +0530

    update

---
+++ php/newsletterform-process.php
@@ -0,0 +1,32 @@
+<?php
+$errorMSG = "";
+
+if (empty($_POST["email"])) {
+    $errorMSG = "Email is required ";
+} else {
+    $email = $_POST["email"];
+}
+
+$EmailTo = "yourname@domain.com";
+$Subject = "New newsletter subscription from Stream landing page";
+
+// prepare email body text
+$Body = "";
+$Body .= "Email: ";
+$Body .= $email;
+$Body .= "\n";
+
+// send email
+$success = mail($EmailTo, $Subject, $Body, "From:".$email);
+
+// redirect to success page
+if ($success && $errorMSG == ""){
+   echo "success";
+}else{
+    if($errorMSG == ""){
+        echo "Something went wrong :(";
+    } else {
+        echo $errorMSG;
+    }
+}
+?>
